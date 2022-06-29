269d7f5dd437381ae9b0e03ce6f8132d217eae6e
Mike <mike@imrsv.ai>
2022-06-29 11:50:34 -0500

    change app to reflect the bug report template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { MapContainer } from 'react-leaflet';
+import React, { StrictMode } from 'react';
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { Marker } from 'react-leaflet/Marker';
+import { Popup } from 'react-leaflet/Popup';
+import { TileLayer } from 'react-leaflet/TileLayer';
 
-function App() {
-
+function SimpleExample(props: any) {
   return (
-    <MapContainer/>
-  )
+    <StrictMode>
+      <MapContainer center={props.position} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={props.position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </StrictMode>
+  );
 }
 
-export default App
+export default function App() {
+  return <SimpleExample position={{ lat: 51.505, lng: -0.09 }} />;
+}
